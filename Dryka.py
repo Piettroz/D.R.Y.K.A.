a3b929dc179bf09dead280b233f4eee04a6a7c2e
Piettro <piettro2010@gmail.com>
2025-10-12 13:04:56 -0300

    Reconhecimento de voz

--- Dryka.py
+++ Dryka.py
@@ -1,9 +1,10 @@
 import pyttsx3
 import datetime
+import speech_recognition as sr
 
 texto_fala = pyttsx3.init()
 
-def falar(*audio):
+def falar(audio):
     rate = texto_fala.getProperty('rate')
     texto_fala.setProperty("rate", 160)              #alteração da velocidade da fala
     voices = texto_fala.getProperty('voices')
@@ -16,14 +17,65 @@ def tempo():
     falar("Agora são")
     falar(Tempo)
 
+'''         TAREFA  0.0:
+
+    trasformar o mês em um nome referente a sua data.
+    e fazer a dryka pronunciar esse nome!
+    ex: mês 8 == agosto
+
+'''
+
 def data():
     Data = datetime.datetime.now()
     ano = int(Data.year)
     mes = int(Data.month)
     dia = int(Data.day)
     falar("a data atual é")
-    falar(f" dia {dia}. do mês {mes}. de {ano}")
+    falar(f" {dia}. do mês {mes}. de {ano}")
 
-tempo()
-data()
+def bem_vindo():
+    falar("Olá mestre. bem vindo de volta!")
+    tempo()
+    data()
+
+    hora = datetime.datetime.now().hour
+                                                     
+    if hora >= 5 and hora < 12:                         
+        falar("bom dia mestre!")
+    elif hora >= 12 and hora < 18:
+        falar("boa tarde mestre!")
+    elif hora >= 18 and hora <= 24:
+        falar("boa noite mestre!")
+    else:                                            
+        falar("Ola Bat man")                           #jovem mocergo
+  
+    falar("dryka a sua disposição! diga-me, como posso ajuda-lo!")
+
+def microfone():
+    r = sr.Recognizer()
+    
+    with sr.Microphone() as source:
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Reconhecendo...")
+        comando = r.recognize_google(audio, language='pt-br')
+        print(comando)
+    
+    except Exception as e:
+        print(e)
+        falar("por favor repita!")
+
+        return "None"
+    
+    return comando
+
+if __name__ == "__main__":
+    bem_vindo()
+
+    while True:
+        print("Escutando...")
+        comando = microfone().lower()
+
 
